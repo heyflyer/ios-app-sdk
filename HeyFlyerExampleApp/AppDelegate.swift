eb8a609a89ed53c8bc384600067a2f804af34cd4
icanpunit <ican.punit@gmail.com>
2019-11-30 13:32:12 +0530

    first push

--- HeyFlyerExampleApp/AppDelegate.swift
+++ HeyFlyerExampleApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-         setHeyFlyerServerKey.init(setKey: "z2I5xrtiU")
+         setHeyFlyerServerKey.init(setKey: "API_KEY_HERE")
         // Override point for customization after application launch.
         return true
     }
